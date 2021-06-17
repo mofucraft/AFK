7428babfcc1ab34446aad8b033c1858822253eb0
yossy_zip <mattsu.zip@gmail.com>
2021-06-17 15:16:57 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/yiorno/afk/AFK.java

--- src/main/java/com/yiorno/afk/AFK.java
+++ src/main/java/com/yiorno/afk/AFK.java
@@ -8,17 +8,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
-public final class AFK extends JavaPlugin implements Listener {
+public final class AFK extends JavaPlugin {
 
     @Override
     public void onEnable() {
@@ -28,7 +22,7 @@ public final class AFK extends JavaPlugin implements Listener {
         config.load();
 
         getLogger().info("離席管理が起動しました");
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new Event(), this);
 
         BukkitScheduler scheduler = getServer().getScheduler();
         scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
